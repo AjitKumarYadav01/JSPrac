53b5f89327705ae5e53fc4c0daa637c96c04562a
Ajit Kumar Yadav <ajitkumary768@gmail.com>
2025-03-31 12:21:21 +0530

    Datq type Conversion

---
+++ .devcontainer/typeCon.js
@@ -0,0 +1,18 @@
+// let score = undefined;
+// let player = null;
+
+// // console.log(typeof score);
+//  console.log(typeof (score));
+
+// let valueInNum = Number(score);
+// console.log(typeof (valueInNum));
+// console.log(valueInNum);
+
+let isLoggedIn = 1;
+let booleanLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanLoggedIn);
+
+let num = 22;
+let numStr = String(num);
+//let numStr = num.toString();
+console.log( typeof (numStr));
